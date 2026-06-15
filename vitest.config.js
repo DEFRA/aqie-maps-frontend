351f5e9e93b07811331762783076bf144481ba05
Stewart Jumbe <stewarjumbe@gmail.com>
2026-06-15 15:35:08 +0100

    add aliesses for imports

--- vitest.config.js
+++ vitest.config.js
@@ -1,6 +1,15 @@
 import { defineConfig, configDefaults } from 'vitest/config'
+import { fileURLToPath } from 'node:url'
+import path from 'node:path'
+
+const dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '#': path.resolve(dirname, 'src')
+    }
+  },
   test: {
     globals: true,
     environment: 'node',
